5e401d01db93dd61b723e278c88f5ce145312e06
Grahame Grieve <grahameg@gmail.com>
2017-10-26 13:20:45 +1100

    get working

---
+++ R/grahame.R
@@ -0,0 +1,18 @@
+library(jsonlite)
+
+source("R/fhirClient.R")
+
+# connect to the server
+endpoint <- "http://test.fhir.org/r3"
+client <- fhirClient$new(endpoint)
+
+# just get a random patient
+a <- client$read(location = "Patient/example", summaryType = "json")
+a$identifier
+
+# count Patients with gender = male
+
+b <- client$search("Patient", "gender=male", summaryType="count")
+b$total
+
+
